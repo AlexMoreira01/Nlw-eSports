ca16cc9b5e54af33e57b305fb7231da6b9f9446e
Alex Moreira <alex.moreira.andrade@hotmail.com>
2022-09-15 23:57:04 -0300

    Feat: Config Db with SQLite and Prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,25 +2,29 @@ import express from 'express';
 
 const app = express();
 
-app.get('/ads', (request, response) => {
+app.get('/games', (request, response) => {
+    return response.json([]);
+})
+
+app.post('/ads', (request, response) => {
+    return response.status(201).json([]);
+})
+
+app.get('/games/:id/ads', (request, response) => {
+    // const gameId = request.params.id;
+
     return response.json([
-        {
-            id: 1, 
-            name: 'Anuncio1'
-        },
-        {
-            id: 2, 
-            name: 'Anuncio2'
-        },
-        {
-            id: 3, 
-            name: 'Anuncio3'
-        },
-        {
-            id: 4, 
-            name: 'Anuncio4'
-        }
+        {id: 1, name: 'Anuncio1'},
+        {id: 2, name: 'Anuncio2'},
+        {id: 3, name:'Anuncio3'},
+        {id: 4, name: 'Anuncio4'}
     ])
+})
+
+app.get('/ads/:id/discord', (request, response) => {
+    // const adId = request.params.id;
+
+    return response.json([])
 })
 
 app.listen(3333)
