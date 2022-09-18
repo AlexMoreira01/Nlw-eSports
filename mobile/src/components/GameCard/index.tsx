dde6ac2d03865640420c14717a12d3ee6f5d00ec
Alex Moreira <alex.moreira.andrade@hotmail.com>
2022-09-17 22:06:52 -0300

    Feat: Create components-routes   stage-4 complete

--- mobile/src/components/GameCard/index.tsx
+++ mobile/src/components/GameCard/index.tsx
@@ -8,9 +8,11 @@ import { THEME } from '../../theme';
 
 export interface GameCardProps {
     id: string;
-    name: string;
-    ads: string;
-    cover: ImageSourcePropType
+    title: string;
+    _count: {
+        ads: number
+    };
+    bannerUrl: string;
 }
 
 interface Props extends TouchableOpacityProps{
@@ -25,7 +27,7 @@ export function GameCard({ data, ...rest }: Props) {
     >
         <ImageBackground
             style={styles.cover}
-            source={data.cover}
+            source={{ uri: data.bannerUrl}}
 
         >
 
@@ -34,10 +36,10 @@ export function GameCard({ data, ...rest }: Props) {
                 style={styles.footer}
             >
                 <Text style={styles.name}>
-                    {data.name}
+                    {data.title}
                 </Text>
                 <Text style={styles.ads}>
-                    {data.ads} anúncios
+                    {data._count.ads} anúncios
                 </Text>
 
             </LinearGradient>
---
+++ mobile/src/screens/Game/index.tsx
@@ -0,0 +1,93 @@
+import { useEffect, useState } from 'react';
+import { FlatList, Image, TouchableOpacity, View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { useRoute, useNavigation } from '@react-navigation/native';
+import { Entypo } from '@expo/vector-icons';
+
+import LogoImg from '../../assets/logo-nlw-esports.png';
+
+import { THEME } from '../../theme';
+import { styles } from './styles';
+
+import { GameParams } from '../../@types/navigation';
+import { Heading } from '../../components/Heading';
+import { Background } from '../../components/Background';
+import { DuoCard, DuoCardProps } from '../../components/DuoCard';
+
+export function Game() {
+  const [duos, setDuos] = useState<DuoCardProps[]>([])
+
+  // Resgatar informações que veem da rotas
+  const route = useRoute();
+  const navigation = useNavigation();
+  const game = route.params as GameParams;
+
+  function handleGoBack(){
+    navigation.goBack();
+  }
+
+  useEffect(() => {
+    fetch(`http://192.168.1.11:3333/games/${game.id}/ads`)
+    .then(response => response.json())
+    // .then(data=> console.log(data[0].name))
+    .then(data=> setDuos(data))
+  }, [])
+
+  return (
+    <Background>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={handleGoBack}>
+            <Entypo
+              name="chevron-thin-left"
+              color={THEME.COLORS.CAPTION_300}
+              size={20}
+            />
+          </TouchableOpacity>
+
+          <Image
+            source={LogoImg}
+            style={styles.logo}
+          />
+
+          <View style={styles.right}/>
+
+        </View>
+
+        <Image
+          source={{uri: game.bannerUrl}}
+          style={styles.cover}
+          resizeMode="cover"
+
+        />
+
+        <Heading
+          title={game.title}
+          subtitle="Conecte-se e comece a jogar!"
+        />
+
+        <FlatList
+          data={duos}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <DuoCard 
+              data={item} 
+              onConnect={()=> {}}  
+            />
+          )}
+          horizontal
+          style={styles.containerList}
+          // Se usa [ ] para passar mais estilos -- Aqui ira definir um estilo padrao poara a mensagem caso não tenha anuncios
+          contentContainerStyle={[duos.length > 0 ? styles.contentList : styles.emptyListContent]}
+          showsHorizontalScrollIndicator={false}
+          ListEmptyComponent={() => (
+            <Text style={styles.emptyListText}>
+              Não há anuncios publicados ainda!
+            </Text>
+          )}
+        />
+
+      </SafeAreaView>
+    </Background>
+  );
+}
